a05af57d98f727437bdce025267c8e3d4c45daa2
heysourin <sourn07@gmail.com>
2022-11-07 02:20:18 +0530

    Updated

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+import web3modal from "web3modal";
+import ethers from "ethers";
+import { useState, useEffect } from "react";
+import { nftaddress,nftmarketaddress} from "../configr";
+
 
 export default function Home() {
-  return (
-    <div className={styles.container}>Hello</div>
-  )
+  return <div className={styles.container}>Hello</div>;
 }
